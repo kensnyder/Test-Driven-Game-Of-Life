236936571a222cf3f8791fa66f5971d11f4e702d
Ken Snyder <kendsnyder@gmail.com>
2013-07-13 18:02:22 -0600

    Remove QUnit Reference from walkthrough

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,9 +17,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	
-	grunt.registerTask('walkthrough14', function() {
+	grunt.registerTask('walkthrough15', function() {
 		var pages = ['01-fail.html','01-pass.html','02-fail.html','02-pass.html','03-fail.html','03-pass.html','04-fail.html','04-pass.html','05-fail.html','05-pass.html','06-fail.html','06-pass.html','07-fail.html','07-pass.html','08-fail.html','08-pass.html','09-fail.html','09-pass.html','10-fail.html','10-pass.html','11-fail.html','11-pass.html','12-fail.html','12-pass.html'];		
-		var find1 = '<div id="qunit"></div>\n';
+		var find1 = /\/\*\nQUnit[\s\S]+?\*\/\n/;
 		var repl1 = '';
 		var html;
 		for (var i = 0; i < pages.length; i++) {
