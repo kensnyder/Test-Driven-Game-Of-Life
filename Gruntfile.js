6d57ae1a5c69f6631b2c226b97b6cf99187e3d11
Ken Snyder <kendsnyder@gmail.com>
2013-09-22 16:56:10 -0600

    workshop files

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,14 +17,13 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	
-	grunt.registerTask('walkthrough15', function() {
-		var pages = ['01-fail.html','01-pass.html','02-fail.html','02-pass.html','03-fail.html','03-pass.html','04-fail.html','04-pass.html','05-fail.html','05-pass.html','06-fail.html','06-pass.html','07-fail.html','07-pass.html','08-fail.html','08-pass.html','09-fail.html','09-pass.html','10-fail.html','10-pass.html','11-fail.html','11-pass.html','12-fail.html','12-pass.html'];		
-		var find1 = /\/\*\nQUnit[\s\S]+?\*\/\n/;
-		var repl1 = '';
+	grunt.registerTask('workshop', function() {
+		var pages = ['00-start.html','01-fail.html','02-fail.html','03-fail.html','04-fail.html','05-fail.html','06-fail.html','07-fail.html','08a-fail.html','08b-fail.html','08c-fail.html','09-fail.html','10-fail.html','11-fail.html','12-fail.html'];		
 		var html;
 		for (var i = 0; i < pages.length; i++) {
 			html = grunt.file.read('./walkthrough/' + pages[i]);
-			grunt.file.write('./walkthrough/' + pages[i], html.replace(find1, repl1));
+			html = html.replace(/( Game Code \*+)[\s\S]+?(<\/script>)/, '$1\n\n\n\n$2');
+			grunt.file.write('./workshop/' + pages[i].replace('-fail', ''), html);
 		}
 	});
 	
